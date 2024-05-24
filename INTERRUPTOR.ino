1876f8b4967cdd9ac3421a72444a11adc46511aa
xR0NALDx <ronald17.mcm@gmail.com>
2024-05-24 16:59:38 -0500

    Add files via upload

---
+++ INTERRUPTOR.ino
@@ -0,0 +1,158 @@
+#include <WiFi.h>
+
+// El dispositivo Esp32 se conectará a la red wifi que le indiquemos con la respectiva contraseña 
+
+const char* ssid = "SKULL";
+const char* password = "SKULL123";
+
+WiFiServer server(80);
+
+String header; //variable para almacenar el encabezado de la solicitud HTTP
+
+//Instanciamos la clase Slider
+
+int limite = 50;
+int lectura; // lectura de la capacitancia de la placa de aluminio 
+
+const int outPin = 4; // pin de salida
+
+String outState = "apagado"; //variables auxiliar para almacenar el estado actual de la salida
+
+unsigned long lastTime, timeout = 2000;
+
+void setup() {
+  Serial.begin(115200);
+   //Inicializamos el slider indicandole los pines touch.
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, HIGH);
+  
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+
+  Serial.println("");
+  Serial.println("Dispositivo conectado.");
+  Serial.println("Direccion IP: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  
+}
+
+void loop(){
+
+   lectura = touchRead(T8); // pin de entrada sensor tactil
+
+      //al tocar la placa metálica provoca que el valor de lectura sea menor a la de limite indicando que se detectó un cambio en la capacitancia  
+      //provocando así que se cumpla la condición, e indicando al Esp32 que debe encender la bombilla o apagarla 
+      //Esto con un tiempo de espera de 500 milisegundos
+
+    if(lectura < limite){                                 
+      digitalWrite(outPin, !digitalRead(outPin));          
+      delay(500);                                          
+    }
+
+
+
+  WiFiClient client = server.available();   //El ESP32 siempre está escuchando a los clientes entrantes
+  if (client) {
+                               
+    lastTime = millis();
+    
+    Serial.println("Nuevo cliente.");      
+    String currentLine = ""; // Cadena para contener los datos entrantes del cliente          
+    while (client.connected() && millis() - lastTime <= timeout) 
+    {
+      
+      if (client.available()) {        
+        char c = client.read();            
+        Serial.write(c);                  
+        header += c;
+        // Si recibe salto de linea y la currentLine esta vacia,
+        // la peticion http finalizó, se responde:
+        if (c == '\n') {   
+                        
+          if (currentLine.length() == 0) {
+
+            // Los encabezados HTTP siempre comienzan con un código de respuesta (por ejemplo, HTTP/1.1 200 OK) 
+            // y un tipo de contenido para que el cliente sepa lo que viene, luego una línea en blanco:
+            
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+
+            //si el estado del boton del html indica que la bombilla esta "apagada"
+            //la bombilla se apaga
+
+           
+            if(header.indexOf("GET /4/off") >= 0) {  
+              Serial.println("GPIO 4 apagado");
+              outState = "apagado";
+              digitalWrite(outPin, HIGH);                          
+            }  
+            
+            //si el estado del boton del html indica que la bombilla esta "encendida"
+            //la bombilla se enciende
+
+            else if(header.indexOf("GET /4/on") >= 0){
+              Serial.println("GPIO 4 encendido");
+              outState = "encendido";
+              digitalWrite(outPin, LOW); 
+            } 
+
+           
+              
+
+            // Pagina web
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+
+            
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".buttonOff {background-color: #555555;}</style></head>");
+            
+         
+            client.println("<body><h1>Control Remoto</h1>");  // Titulo
+            
+  
+            client.println("<p>Dispositivo: " + outState + "</p>"); // Estado del dispositivo
+                  
+            if (outState=="apagado") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">Encender</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button buttonOff\">Apagar</button></a></p>");
+            } 
+               
+           
+
+            client.println();
+           
+            break;
+          } else { 
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;      
+        }
+      }
+    }
+    
+    header = "";  
+    client.stop();
+    Serial.println("Cliente desconectado.");
+    Serial.println("");
+  }
+  
+ 
+
+}
